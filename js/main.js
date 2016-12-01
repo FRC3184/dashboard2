b506955f75384471df54b1b7ef555077ccb46d83
Nick Schatz <nickbrickmaster@gmail.com>
2016-11-30 21:04:46 -0600

    Size and position are stored in browser localStorage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 var name_map = {};
 var buffer_size = 200;
-var my_grid = [200, 200];
+var my_grid = [100, 100];
 Chart.defaults.global.maintainAspectRatio = false;
+
+var drag_opts = {
+    grid: my_grid,
+    stop: function(evt, ui) {
+        ui.position = roundVector(ui.position, my_grid);
+    }
+};
+var resize_opts = {grid: my_grid};
+
+/*
+ * Rounds both parts of vec_k to nearest roundMagnitude_k
+ * Returns rounded vector
+*/
+function roundVector(vec, round) {
+    var ret = vec;
+    ret.left = Math.round(vec.left / round[0]) * round[0];
+    ret.top = Math.round(vec.top / round[1]) * round[1];
+    return ret;
+}
 
 var update_chart = function(data_t, data_y) {
   this.data.datasets[0].data.push({x: data_t, y: data_y});
@@ -13,14 +32,11 @@ var update_chart = function(data_t, data_y) {
 
 var make_chart = function(data) {
   console.log(data.name);
-  var chart_element = $("<canvas data-name=\"" + data.name + "\" width=\"400px\" height=\"400px\" class=\"chart\"></canvas>");
+  var chart_element = $("<canvas width=\"400px\" height=\"400px\" class=\"chart\"></canvas>");
   chart_element.appendTo("#dashboard");
-  chart_wrapper = $("<div class=\"chart-wrap ui-widget-content\"></div>");
+  chart_wrapper = $("<div class=\"element-wrap ui-widget-content\" data-name=\"" + data.name + "\"></div>");
   chart_element.wrap(chart_wrapper);
 
-  $(".chart-wrap").draggable({grid: my_grid}).resizable({grid: my_grid});
-  console.log(chart_element);
-  console.log(chart_wrapper);
   var chart = new Chart(chart_element, {
     type: "line",
     data: {
@@ -72,4 +88,57 @@ source.addEventListener("action", function(event) {
       make_chart(data);
       break;
   }
+});
+
+function stringifyPosition(position) {
+    return position.top + "," + position.left;
+}
+function destringifyPosition(str) {
+    var obj = {};
+    var split = str.split(",");
+    obj.top = parseInt(split[0]);
+    obj.left = parseInt(split[1]);
+    return obj;
+}
+
+$(document).ready(function() {
+
+    $("#save-button").click(function() {
+        $(".element-wrap").each(function(i, element) {
+            element = $(element);  //Why jQuery doesn't return these as jQuery objects is beyond me.
+            var name = element.data("name");
+            localStorage.setItem(name + ".position", stringifyPosition(element.position()));
+            localStorage.setItem(name + ".size", element.width() + "," + element.height());
+        });
+    });
+    $("#refresh-button").click(function() {
+        source.close();
+        location.reload();
+    });
+
+    setTimeout(function() {
+        $(".element-wrap").draggable(drag_opts).resizable(resize_opts).draggable("disable");
+
+        $("#editable-check").change(function() {
+            $(".element-wrap").draggable(this.checked ? "enable" : "disable");
+            $(".element-wrap").resizable(this.checked ? "enable" : "disable");
+        });
+
+        console.log("Loading positions...");
+        $(".element-wrap").each(function(i, element) {
+            element = $(element);  //Why jQuery doesn't return these as jQuery objects is beyond me.
+            var name = element.data("name");
+            var position = localStorage.getItem(name + ".position");
+            var size = localStorage.getItem(name + ".size");
+            if (position !== null) {
+                var newPos = destringifyPosition(position);
+                element.css({top: newPos.top, left: newPos.left});
+            }
+            if (size !== null) {
+                var newSize = destringifyPosition(size);
+                element.css({width: newSize.top, height: newSize.left});
+            }
+        });
+        console.log("Loaded positions");
+    }, 500)
 });
